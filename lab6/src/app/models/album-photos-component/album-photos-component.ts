34d7d44f6d8bed3b2e4cba1468e2119b05bf1709
0m1rum9 <0m1rum9@gmail.com>
2026-03-02 16:02:22 +0500

    Lab6

---
+++ lab6/src/app/models/album-photos-component/album-photos-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-photos-component',
+  imports: [],
+  templateUrl: './album-photos-component.html',
+  styleUrl: './album-photos-component.css',
+})
+export class AlbumPhotosComponent {
+
+
+}
